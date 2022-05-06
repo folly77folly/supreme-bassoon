f3b88e3cb281ba08075abe399b7ac1c9ab4a2998
folly77folly <folly77folly@gmail.com>
2022-05-06 02:07:03 +0100

    added color crud

---
+++ app/Http/Controllers/Api/Admin/ColorController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\Color;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\EditColorRequest;
+use App\Http\Requests\SaveColorRequest;
+
+class ColorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $colors = Color::status()->get();
+        // dd($colors);
+        return $this->apiResponse->successWithData($colors);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SaveColorRequest $request)
+    {
+        //
+        $formData = $request->validated();
+        $color = Color::create($formData);
+        return $this->apiResponse->created($color, 'colors saved successfully');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $color = Color::findOrFail($id);
+        return $this->apiResponse->created($color, 'color retrieved');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(EditColorRequest $request, $id)
+    {
+        //
+        $color = Color::find($id);
+        $color->fill($request->all())->save();
+        return $this->apiResponse->successWithData($color, 'color updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Color::findOrFail($id);
+        Color::destroy($id);
+        return $this->apiResponse->success('color deleted');
+        
+    }
+}
---
+++ app/Models/Color.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Color extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [ 
+        'status' => 'boolean',
+    ];
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeStatus($query)
+    {
+        return $query->where('status', true);
+    }
+}
