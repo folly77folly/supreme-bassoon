51bd7236b5916c577ad55fed977ce92785693a40
folly77folly <folly77folly@gmail.com>
2022-05-24 03:07:51 -0700

    Comment resolved

--- config/app.php
+++ config/app.php
@@ -172,7 +172,6 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\HelperServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
